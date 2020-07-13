b5c5a7f3fa51d6cf3bf286a3d2dc9a8a9cdb9961
Aizhan Zhanakova <aijan.janakova@gmail.com>
2020-07-13 14:46:16 -0400

    commit

---
+++ src/Replit/StringCharAt.java
@@ -0,0 +1,21 @@
+package Replit;
+/*
+using charAt method output the 3rd letter of txt string then the 5th letter then the 6th letter.
+
+use print not println.
+ */
+import java.util.Scanner;
+
+public class StringCharAt {
+//Muhtar's code
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String txt = s.next();
+        //your code here
+
+        System.out.print(txt.charAt(2));
+        System.out.print(txt.charAt(4));
+        System.out.print(txt.charAt(5));
+    }
+
+}
